40a0afb5cd0391bc5155235d9ff348e2b0968d25
nattawut8942 <natthawut8942@gmail.com>
2024-11-17 14:46:47 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
+    <div class="container">
     <h1>This is an about page</h1>
+    <p>Nattawut Yodprasert</p>
+    <button type="button" class="btn btn-success ">Press here</button> &nbsp;
+    <button type="button" class="btn btn-success">Success</button>
   </div>
+  </div>
+  
+
 </template>
+
+
+
+
