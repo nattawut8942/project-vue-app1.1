6bbf295987dcd3938763aa2495fb14df9e01c325
nattawut8942 <natthawut8942@gmail.com>
2024-11-17 14:46:38 +0700

    Create Product.vue

---
+++ src/views/Product.vue
@@ -0,0 +1,67 @@
+<template lang="">
+    <div>
+        <div class="container text-center mx-auto">
+      <div class="row">
+        <div class="col-md-3 p-3 ">
+            <img src="https://prod-thailand-res.popmart.com/default/20240930_111001_278862____1_____1200x1200.jpg" alt="" style="width: 200px; heigh: 250px; display: block; cursor: crosshair;">
+          <!-- <img src="img/iPhone_14_Pro_Deep_Purple_PDP_Im.png " width="200" height="250" alt="" class="img-fluid my-5 "> -->
+          <h3>CRYBABY CHEER UP, BABY! SERIES-Plush Doll</h3>
+        </div>
+        <div class="col-md-3 p-3">
+            <img src="https://prod-thailand-res.popmart.com/default/20241021_165547_329339____1_____1200x1200.jpg" alt="" style="width: 200px; heigh: 250px; display: block; cursor: crosshair;">
+          <!-- <img src="img/iPhone_14_Pro_Max_Deep_Purple_PD.png" width="200" height="250" alt="" class="img-fluid my-5"> -->
+          <h3>POP BEAN CRYBABY The Elephant Set</h3>
+        </div>
+        <div class="col-md-3 p-3">
+            <img src="https://prod-thailand-res.popmart.com/default/20241014_114305_579076____1_____1200x1200.jpg" alt="" style="width: 200px; heigh: 250px; display: block; cursor: crosshair;">
+          <!-- <img src="img/iPhone_15_Pro_Blue_Titanium_1-.png" width="200" height="250" alt="" class="img-fluid my-5"> -->
+          <h3>CRYBABY HELLO THAILAND SERIES-Plush Doll</h3>
+        </div>
+        <div class="col-md-3 p-3">
+            <img src="https://prod-eurasian-res.popmart.com/default/20240617_143204_955539____4_____1200x1200.jpg" alt="" style="width: 200px; heigh: 250px; display: block; cursor: crosshair;">
+          <!-- <img src="img/iPhone_15_Pro_Max_Natural_Titanium_PDP_Image_Position-1__GBEN_967e09a2-12dc-407c-a315-e56c17ad5dfc-removebg-preview.png" width="200" height="250" alt="" class="img-fluid my-5"> -->
+          <h3>CRYBABY Sunset Concert Series-Plush Doll</h3>
+        </div>
+          <div class="col-md-3 p-3">
+            <img src="https://prod-thailand-res.popmart.com/default/20240924_095748_398503____1_____1200x1200.jpg" alt="" style="width: 200px; heigh: 250px; display: block; cursor: crosshair;">
+            <!-- <img src="img/iPad_s10-removebg-preview.png" width="200" height="250" alt="" class="img-fluid my-4"> -->
+            <h3>CRYBABY Crying Again Series Figures</h3>
+          </div>
+          <div class="col-md-3 p-3">
+            <img src="https://prod-eurasian-res.popmart.com/default/20240304_164111_296715__1200x1200.jpg" alt="" style="width: 200px; heigh: 250px; display: block; cursor: crosshair;">
+            <!-- <img src="img/iPad_10th_Gen_Cellular_Blue_5G_2-square_medium-removebg-preview.png" width="200" height="250" alt="" class="img-fluid my-4"> -->
+            <h3>CRYBABY × Powerpuff Girls Series Figures</h3>
+          </div>
+          <div class="col-md-3 p-3">
+            <img src="https://prod-eurasian-res.popmart.com/default/20240428_143432_982624__1200x1200.jpg" alt="" style="width: 200px; heigh: 250px; display: block; cursor: crosshair;">
+            <!-- <img src="img/iPad_10-removebg-preview (1).png" width="200" height="250" alt="" class="img-fluid my-4"> -->
+            <h3>CRYBABY CHEER UP, BABY! SERIES-Plush Doll</h3>
+          </div>
+          <div class="col-md-3 p-3">
+            <img src="https://prod-eurasian-res.popmart.com/default/20240514_093114_772956____11_hegolgqlss_1200x1200_____1200x1200.jpg" alt="" style="width: 200px; heigh: 250px;  display: block; cursor: crosshair;">
+            <!-- <img src="img/iPad_10th_Gen_Cellular_Blue_5G_2-square_medium-removebg-preview.png " width="200" height="250" alt="" class="img-fluid my-4"> -->
+            <h3>CRYBABY Monster Tears Series</h3>
+          </div>
+
+          
+          
+        </div>
+       
+        </div>
+    </div>
+    
+
+    
+</template>
+<script>
+export default {
+    name: 'Product',
+  components: {
+    
+  }
+    
+}
+</script>
+<style lang="">
+    
+</style>
