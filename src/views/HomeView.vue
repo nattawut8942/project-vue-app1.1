3fe88fb3fe11650b287238362819e614ecc63a31
nattawut8942 <natthawut8942@gmail.com>
2024-11-17 14:46:40 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
+    <div class ="container">
+    <h1>Nattawut Yodprasert</h1>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-primary">Hello Bootstrap 5 </h1>
+    
+  </div>
   </div>
 </template>
 
