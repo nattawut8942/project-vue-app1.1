27eb766e6f77ab28293b172e69867f0a3375a705
nattawut8942 <natthawut8942@gmail.com>
2024-11-17 14:46:49 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,45 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/Contact',
+    name: 'Contact',
+    
+    component: () => import('../views/Contact.vue')
+  },
+  {
+    path: '/Product',
+    name: 'Product',
+    
+    component: () => import('../views/Product.vue')
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+  
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/Register',
+    name: 'Register',
+  
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/Grade',
+    name: 'Grade',
+  
+    component: () => import('../views/Gradecal.vue')
+  },
+  {
+    path: '/Addproduct',
+    name: 'Addproduct',
+  
+    component: () => import('../views/Addproduct.vue')
   }
+
+
 ]
 
 const router = createRouter({
